f9de875ac7c50191d713762c4139b1a80deeb345
anil.bhaskar <anil.bhaskar@rakuten.com>
2019-07-29 15:28:01 +0530

    siri ganesh

---
+++ src/main/java/in/taskoo/TaskooApplication.java
@@ -0,0 +1,13 @@
+package in.taskoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskooApplication.class, args);
+	}
+
+}
